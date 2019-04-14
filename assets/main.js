7ab2f7be0a07992069c82616cfbbcf2c5128f0c7
jasongoesplaces <jasongoesplaces@gmail.com>
2019-04-14 12:21:34 -0400

    our story page update

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,25 @@
-$('#toggle').click(function() {
-  $(this).toggleClass('active');
-  $('#overlay').toggleClass('open');
- });
+// Mobile Dropdown Menu
+// $('#toggle').click(function() {
+//   $(this).toggleClass('active');
+//   $('#overlay').toggleClass('open');
+//  });
+
+ // Timeline
+ var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
